ab2143a668fe6c6d626d39e621eff943801637b3
jdpenuliar <jaydz.penuliar@gmail.com>
2017-02-01 17:07:44 -0800

    before push and branch out

---
+++ Assets/consolePrinter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class consolePrinter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int hahaNumber;
+		hahaNumber = 1;
+		print ("haha");
+		print ("this should be my haha\n lolllllll");
+		print (hahaNumber);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# gethome
+# gethome (Work in Progress)
+The goal of this project is to get fundamentals on virtual reality in order to build toward to an inovating medical related app.
+This game's objective is to get to home point when you spawn on a random contorlled radius.
+
